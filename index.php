31405db04ee63062d7264a9db878f8d214383ccf
MironSkillzzz <55944639+MironSkillzzz@users.noreply.github.com>
2021-11-30 04:16:00 +0300

    rabotaiblyat

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Rubik:wght@700&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        
+        <div class="header__top">
+          <img class="logo" src="images/logo.png" alt="logo">
+          <nav class="menu">
+            <ul class="menu__list">
+              <li class="menu__item"><a class="menu__link" href="#">Главная</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">О нас</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">Мастера</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">Галерея</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">Контакты</a></li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="header__content">
+          <h1 class="header__title">Делаем качественные татуировки за короткий срок</h1>
+          <a class="header__btn" href="#">Записаться</a>
+        </div>
+
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="about">
+      <div class="container">
+        <h2 class="title title-black">О нас</h2>
+        <div class="about__inner">
+          <div class="about__content">
+            <p class="about__content-text">Есть люди, которые выходят за рамки своей деятельности и творят нечто
+            крутое, эксклюзивное влияя на окружающий мир и задавая тенденции. Мы
+            собрали именно таких - самых талантливых мастеров своего дела со всех уголков мира. Так появилась студия DRAGON -
+            неординарный проек, где выше
+            всего ценится индивидувльность и высокий профессионализм.</p>
+            <p class="about__content-text">Наша работа ведется только профессиональными инструментами и
+            одноразовыми расходниками премиум уровня. Стерилизация, дезинфекция
+            оборудования, использование барьерной защиты соблюдается в полном объеме,
+            что исключает возможность заражения чем либо и делает процесс татуировки
+            максимально безболезненным и безопасным.</p>
+            <p class="about__content-text">Мы проводим бесплатные консультации для наших клиентов, помогаем определиться со стилем, сожетом тату. Также мы помогаем
+            выбрать или создать с нуля эскиз, сделав его максимально индивидуальным и грамотно
+            расположенным на теле.</p>
+          </div>
+          <img class="about__img" src="images/about.jpg" alt="about">
+        </div>
+      </div>
+    </section>
+
+    <section class="masters">
+      <div class="container">
+        <h2 class="title title-black">Наши мастера</h2>
+        <div class="masters__inner">
+          <div class="masters__item">
+            <img class="masters__img" src="images/alexander.jpg" alt="">
+            <h3 class="masters__title">Александр</h3>
+            <p class="masters__subtitle">Опыт работы 8 лет</p>
+            <div class="masters__social">
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/vk.svg" alt="vk"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/instagram.svg" alt="instagram"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/twitter.svg" alt="twitter"></a>
+            </div>
+          </div>
+          <div class="masters__item">
+            <img class="masters__img" src="images/marina.jpg" alt="">
+            <h3 class="masters__title">Марина</h3>
+            <p class="masters__subtitle">Опыт работы 6 лет</p>
+            <div class="masters__social">
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/vk.svg" alt="vk"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/instagram.svg" alt="instagram"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/twitter.svg" alt="twitter"></a>
+            </div>
+          </div>
+          <div class="masters__item">
+            <img class="masters__img" src="images/david.jpg" alt="">
+            <h3 class="masters__title">Давид</h3>
+            <p class="masters__subtitle">Опыт работы 13 лет</p>
+            <div class="masters__social">
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/vk.svg" alt="vk"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/instagram.svg" alt="instagram"></a>
+              <a class="masters__social-link" href="#"><img class="masters__social-img" src="images/twitter.svg" alt="twitter"></a>
+            </div>
+          </div>
+          <div class="masters__item">
+            <p class="masters__text">Наши мастера являются хорошо обученными профессионалами, которые помогут разработать дизайн вашей будущей татуировки.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="gallery">
+      <div class="container">
+        <h2 class="title title-black">Галерея работ</h2>
+        <div class="gallery__inner">
+          <img class="gallery__img" src="images/gallery-1.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-2.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-3.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-4.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-5.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-6.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-7.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-8.jpg" alt="tattoo">
+          <img class="gallery__img" src="images/gallery-9.jpg" alt="tattoo">
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer class="footer">
+    <div class="container">
+      <h2 class="title">Контакты</h2>
+      <div class="footer__inner">
+
+        <div class="footer__left">
+          <p class="footer__desc">Оставьте свой телефон и мы ответим на любые вопросы.</p>
+
+          <?php
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\SMTP;
+            use PHPMailer\PHPMailer\Exception;
+
+            require "libs/PHPMailer/Exception.php";
+            require "libs/PHPMailer/PHPMailer.php";
+            require "libs/PHPMailer/SMTP.php";
+  
+            if (isset($_POST["submit"])) {
+              $name = $_POST["name"];
+              $phone = $_POST["phone"];
+
+              $errors = [];
+
+              if (empty($phone)) {
+                $errors["phone"] = "is-invalid";
+              }
+
+              if (empty($errors)) {
+                $mail = new PHPMailer(true);
+                $mail->isSMTP();  
+                $mail->Host = "smtp.mail.ru";
+                $mail->SMTPAuth   = true;
+                $mail->Username   = "tattoo-site@mail.ru";
+                $mail->Password   = "5i7wZFmUNVG0bwf7jU0q";
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port = 465;
+
+                $mail->setFrom("tattoo-site@mail.ru", 'Tattoo');
+                $mail->addAddress("tattoo-site@mail.ru", 'Mikhail');
+
+                $mail->isHTML(true);
+                $mail->Subject = "Tattoo";
+                $mail->Body = "Имя: $name<br>Телефон: $phone";
+                
+                try {
+                  $mail->send();
+                ?>
+                  <div class="alert alert-dangeon" role="alert">
+                    Сообщение отправлено
+                  </div>
+                <?php
+                } catch (Exception $exception) {
+                ?>
+                  <div class="alert alert-dangeon" role="alert">
+                  <?= $exception->getMessage() ?>
+                  </div>
+                <?php
+                }
+              } else {
+                  ?>
+                    <div class="alert alert-dangeon" role="alert">
+                    Заполните поле
+                    </div>
+                  <?php
+              }
+            }
+          ?>
+
+          <form class="footer__form" action="index.php" method="post">
+            <input class="footer__input" name="name" placeholder="Ваше имя" type="text">
+            <input class="footer__input <?= $errors["phone"] ?? "main" ?>" name="phone" placeholder="Ваш номер телефона" type="tel" required>
+            <button class="footer__btn" name="submit" type="submit">Отправить</button>
+          </form>
+          
+
+        </div>
+
+        <div class="footer__right">
+          <div class="footer__item">
+            <p class="footer__text">Наш телефон:</p>
+            <p class="footer__text-desc">+7 950 255 35 45</p>
+          </div>  
+          <div class="footer__item">
+            <p class="footer__text">Наш адрес:</p>
+            <p class="footer__text-desc">ул. Красноармейская, д. 24</p>
+          </div>
+          <div class="footer__item">
+            <p class="footer__text">Подписывайтесь на нас:</p>
+            <div class="footer__social">
+              <a href="#" class="footer__social-link">
+                <img class="footer__social-img" src="images/vk.svg" alt="">
+              </a>
+              <a href="#" class="footer__social-link">
+                <img class="footer__social-img" src="images/instagram.svg" alt="">
+              </a>
+              <a href="#" class="footer__social-link">
+                <img class="footer__social-img" src="images/twitter.svg" alt="">
+              </a>
+              <a href="#" class="footer__social-link">
+                <img class="footer__social-img" src="images/twitter.svg" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
